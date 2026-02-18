1c3633316f5a51600a79ad1b284cc4c0f2101fd4
Nithin <nithinteja.43@gmail.com>
2026-02-18 12:26:23 +0530

    Updated code

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'cypress';
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
-import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/dist/subpath-entrypoints/esbuild';
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://www.automationexercise.com',
-    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: "https://www.automationexercise.com",
+    specPattern: "cypress/e2e/**/*.feature",
+    supportFile: "cypress/support/e2e.ts",
+    watchForFileChanges: false,
+    chromeWebSecurity: false,
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
       on(
-        'file:preprocessor',
+        "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
@@ -18,4 +21,10 @@ export default defineConfig({
       return config;
     },
   },
+  downloadsFolder: "cypress/downloads",
+  video: true,
+  pageLoadTimeout: 60000,
+  requestTimeout: 15000,
+  defaultCommandTimeout: 10000,
+  screenshotOnRunFailure: true,
 });
